7e9c2989e243d2e3d1ad4567e58a865484adb1e4
Kiran122-ux <kirankumarbalaji98@gmail.com>
2026-01-27 13:18:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// string
+
+
+myVariable = "Web Developer"
+
+console.log(myVariable)
+
+console.log(typeof myVariable)
+
+// length - starts from 1
+console.log(myVariable.length)
+
+// charAt -starts from 0
+console.log(myVariable.charAt(4)) 
+
+//index of - starts from 0
+console.log(myVariable.indexOf("e"))
+
+//last index of - starts from 0
+console.log(myVariable.lastIndexOf("e"))
+
+//slice
+console.log(myVariable.slice(0,3) )
+
+//upperCase - Capital letters
+console.log(myVariable.toUpperCase())
+
+
+//lowerCase - small letters
+console.log( myVariable.toLowerCase())
+
+// include
+console.log( myVariable.includes("san") )
+
+// split
+console.log( myVariable.split(" ") )
+
+//replace
+console.log( myVariable.replace("Web", "Frontend") )
