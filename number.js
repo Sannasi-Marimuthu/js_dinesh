308bf383460c6f54af600cbfef7feb85b3252240
Kiran122-ux <kirankumarbalaji98@gmail.com>
2026-01-27 13:33:39 +0530

    number topic

---
+++ number.js
@@ -0,0 +1,47 @@
+// Number
+
+const myNumber = 42;
+
+console.log(myNumber)
+
+console.log(typeof myNumber)
+
+
+const myFloat = 42.0;
+
+console.log(myFloat)
+
+console.log(typeof myFloat)
+
+console.log(myNumber === myFloat)
+
+
+
+// string to number 
+const myNumber2 = "42"
+
+console.log(myNumber2 + 5)
+
+console.log(Number(myNumber2) + 8)
+
+
+console.log(Number("5")+5)
+
+
+console.log(Number("abc")+5)
+//  NaN - Not a Number
+
+console.log(Number(undefined)+5)  //nan
+
+console.log(Number(true)+5)   //1
+
+console.log(Number(false)+5)  // 0
+
+
+// Maths
+
+
+
+
+
+
