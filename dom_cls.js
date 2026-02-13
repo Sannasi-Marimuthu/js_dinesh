20d4d4699378ef503f0dcb4002ed9df7de7751f6
Kiran122-ux <kirankumarbalaji98@gmail.com>
2026-02-13 11:42:45 +0530

    dom

---
+++ dom_cls.js
@@ -0,0 +1,14 @@
+// DOM - Document Object Model
+
+// Change the content
+// Add Elements
+// Remove Elements
+// CHange Styles
+// Reacting to Events
+
+document.getElementById("para1").innerText = "This is the first paragraph";
+
+document.getElementById("para3").innerHTML = " <i style='color: green; font-weight: bold;' >Update para</i> "
+
+// onClick, onMouseOver, onMouseOut, onChange, onKeyDown, onKeyUp, onLoad, onSubmit
+
